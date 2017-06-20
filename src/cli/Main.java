83241a07cd3ae285cebee8387feee6cbc07ec0fa
SamyaDaleh <samya.daleh@freenet.de>
2017-06-20 22:13:39 +0200

    reformatting help output

--- src/cli/Main.java
+++ src/cli/Main.java
@@ -1,4 +1,5 @@
 package cli;
+
 import java.io.IOException;
 import java.text.ParseException;
 
@@ -25,19 +26,19 @@ class Main {
       System.out.println(
         "Please pass at least 3 parameters: [grammar file] [input string] "
           + "[parsing algorithm] [<optional parameters>]");
+      System.out.println("Parsing algorithm can be one of: \n   cfg-cyk"
+        + "\n   cfg-cyk-extended" + "\n   cfg-cyk-general" + "\n   cfg-earley"
+        + "\n   cfg-leftcorner" + "\n   cfg-leftcorner-chart"
+        + "\n   cfg-topdown" + "\n   cfg-shiftreduce" + "\n   cfg-unger"
+        + "\n   pcfg-astar" + "\n   tag-cyk" + "\n   tag-earley"
+        + "\n   srcg-cyk" + "\n   srcg-cyk-extended" + "\n   srcg-earley");
       System.out.println(
-        "Parsing algorithm can be: cfg-cyk, " + "cfg-cyk-extended, " + "cfg-cyk-general, " + "cfg-earley, " 
-          + "cfg-topdown, " + "cfg-shiftreduce, " + "cfg-leftcorner, " + "cfg-leftcorner-chart, " 
-          + "cfg-unger, " + "pcfg-astar, " + "tag-cyk, " + "tag-earley, " + "srcg-cyk, "
-          + "srcg-cyk-extended, " + "srcg-earley");
-      System.out.println(
-        "Optional parameters can be: --success : prints a trace only of items "
-          + "that lead to a goal item.");
-      System.out.println(
-        "Optional parameters can be: --please : if a grammar doesn't fit an "
+        "Optional parameters can be: \n   --success : prints a trace only of items "
+          + "that lead to a goal item."
+          + "\n   --please : if a grammar doesn't fit an "
           + "algorithm, ask me to convert it. No promises.");
       System.out.println(
-        "example: ..\\resources\\grammars\\anbn.cfg \"a a b b\" cfg-topdown");
+        "example: ..\\resources\\grammars\\anbn.cfg \"a a b b\" cfg-topdown --success");
       return;
     }
     String grammarFile = args[0];
@@ -63,12 +64,12 @@ class Main {
     Pcfg pcfg;
     if (grammarFile.endsWith(".cfg")) {
       cfg = GrammarParser.parseCfgFile(grammarFile);
-      if (algorithm.startsWith("cfg")){
+      if (algorithm.startsWith("cfg")) {
         cfg = ggc.checkAndMayConvertToCfg(cfg, algorithm);
         if (cfg != null) {
           schema = gdrc.convertToSchema(cfg, w, algorithm);
         }
-      } else  if (algorithm.startsWith("tag")){
+      } else if (algorithm.startsWith("tag")) {
         tag = ggc.checkAndMayConvertToTag(cfg, algorithm);
         if (tag != null) {
           schema = gdrc.convertToSchema(tag, w, algorithm);
@@ -84,42 +85,43 @@ class Main {
           schema = gdrc.convertToSchema(srcg, w, algorithm);
         }
       } else {
-        System.out.println("I don't know that formalism, please check the spelling.");
+        System.out
+          .println("I don't know that formalism, please check the spelling.");
         return;
       }
-    } else 
-      if (grammarFile.endsWith(".pcfg")) {
-        pcfg = GrammarParser.parsePcfgFile(grammarFile);
-        if (algorithm.startsWith("cfg")){
-          cfg = ggc.checkAndMayConvertToCfg(pcfg, algorithm);
-          if (cfg != null) {
-            schema = gdrc.convertToSchema(cfg, w, algorithm);
-          }
-        } else  if (algorithm.startsWith("tag")){
-          tag = ggc.checkAndMayConvertToTag(pcfg, algorithm);
-          if (tag != null) {
-            schema = gdrc.convertToSchema(tag, w, algorithm);
-          }
-        } else if (algorithm.startsWith("pcfg")) {
-          pcfg = ggc.checkAndMayConvertToPcfg(pcfg, algorithm);
-          if (pcfg != null) {
-            schema = gdrc.convertToSchema(pcfg, w, algorithm);
-          }
-        } else if (algorithm.startsWith("srcg")) {
-          srcg = ggc.checkAndMayConvertToSrcg(pcfg, algorithm);
-          if (srcg != null) {
-            schema = gdrc.convertToSchema(srcg, w, algorithm);
-          }
-        } else {
-          System.out.println("I don't know that formalism, please check the spelling.");
-          return;
+    } else if (grammarFile.endsWith(".pcfg")) {
+      pcfg = GrammarParser.parsePcfgFile(grammarFile);
+      if (algorithm.startsWith("cfg")) {
+        cfg = ggc.checkAndMayConvertToCfg(pcfg, algorithm);
+        if (cfg != null) {
+          schema = gdrc.convertToSchema(cfg, w, algorithm);
         }
-      }else if (grammarFile.endsWith(".tag")) {
-        tag = GrammarParser.parseTagFile(grammarFile);
-      if (algorithm.startsWith("cfg")){
+      } else if (algorithm.startsWith("tag")) {
+        tag = ggc.checkAndMayConvertToTag(pcfg, algorithm);
+        if (tag != null) {
+          schema = gdrc.convertToSchema(tag, w, algorithm);
+        }
+      } else if (algorithm.startsWith("pcfg")) {
+        pcfg = ggc.checkAndMayConvertToPcfg(pcfg, algorithm);
+        if (pcfg != null) {
+          schema = gdrc.convertToSchema(pcfg, w, algorithm);
+        }
+      } else if (algorithm.startsWith("srcg")) {
+        srcg = ggc.checkAndMayConvertToSrcg(pcfg, algorithm);
+        if (srcg != null) {
+          schema = gdrc.convertToSchema(srcg, w, algorithm);
+        }
+      } else {
+        System.out
+          .println("I don't know that formalism, please check the spelling.");
+        return;
+      }
+    } else if (grammarFile.endsWith(".tag")) {
+      tag = GrammarParser.parseTagFile(grammarFile);
+      if (algorithm.startsWith("cfg")) {
         System.out.println("I can't parse with a less expressive formalism.");
         return;
-      } else  if (algorithm.startsWith("tag")){
+      } else if (algorithm.startsWith("tag")) {
         tag = ggc.checkAndMayConvertToTag(tag, algorithm);
         if (tag != null) {
           schema = gdrc.convertToSchema(tag, w, algorithm);
@@ -127,17 +129,19 @@ class Main {
       } else if (algorithm.startsWith("pcfg")) {
         System.out.println("I can't parse with a less expressive formalism.");
       } else if (algorithm.startsWith("srcg")) {
-        System.out.println("I can't convert a tree language to a string language.");
+        System.out
+          .println("I can't convert a tree language to a string language.");
         return;
       } else {
-        System.out.println("I don't know that formalism, please check the spelling.");
+        System.out
+          .println("I don't know that formalism, please check the spelling.");
         return;
       }
     } else if (grammarFile.endsWith(".srcg")) {
       srcg = GrammarParser.parseSrcgFile(grammarFile);
-      if (algorithm.startsWith("cfg")){
+      if (algorithm.startsWith("cfg")) {
         System.out.println("I can't parse with a less expressive formalism.");
-      } else  if (algorithm.startsWith("tag")){
+      } else if (algorithm.startsWith("tag")) {
         System.out.println("I can't parse with a less expressive formalism.");
       } else if (algorithm.startsWith("pcfg")) {
         System.out.println("I can't parse with a less expressive formalism.");
@@ -147,7 +151,8 @@ class Main {
           schema = gdrc.convertToSchema(srcg, w, algorithm);
         }
       } else {
-        System.out.println("I don't know that formalism, please check the spelling.");
+        System.out
+          .println("I don't know that formalism, please check the spelling.");
         return;
       }
     }
@@ -163,15 +168,15 @@ class Main {
     }
     if (schema != null) {
       if (algorithm.startsWith("tag")) {
-        Tree derivedTree = ChartToTreeConverter.tagToDerivatedTree(
-          deduction, schema.getGoals(), tag);
+        Tree derivedTree = ChartToTreeConverter.tagToDerivatedTree(deduction,
+          schema.getGoals(), tag);
         if (derivedTree != null) {
           new DisplayTree(new String[] {derivedTree.toString()});
         }
       }
       if (algorithm.startsWith("cfg")) {
-        Tree derivedTree = ChartToTreeConverter.cfgToDerivatedTree(
-          deduction, schema.getGoals(), algorithm.substring(4));
+        Tree derivedTree = ChartToTreeConverter.cfgToDerivatedTree(deduction,
+          schema.getGoals(), algorithm.substring(4));
         if (derivedTree != null) {
           new DisplayTree(new String[] {derivedTree.toString()});
         }
