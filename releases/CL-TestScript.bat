1265ec8a77e6e6f1cf22dcefdd122cc5c19bc4f6
SamyaDaleh <samya.daleh@freenet.de>
2018-05-28 17:39:00 +0200

    new file that runs consecutive calls to test toolbox jar

---
+++ releases/CL-TestScript.bat
@@ -0,0 +1,4 @@
+call .\CL-Toolbox.bat anbn.cfg "a a b b" cfg-topdown
+call .\CL-Toolbox.bat abc.pcfg "a b c" pcfg-astar
+call .\CL-Toolbox.bat ancb.tag "a c b" tag-earley
+call .\CL-Toolbox.bat anbn.srcg "a a b b" srcg-earley
