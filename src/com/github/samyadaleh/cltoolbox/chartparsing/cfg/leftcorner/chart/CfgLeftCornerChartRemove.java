126f48f7791b2c4e1550dc9f2bf0907e05c65ac4
SamyaDaleh <samya.daleh@freenet.de>
2018-08-31 09:22:05 +0200

    refactoring

--- src/com/github/samyadaleh/cltoolbox/chartparsing/cfg/leftcorner/chart/CfgLeftCornerChartRemove.java
+++ src/com/github/samyadaleh/cltoolbox/chartparsing/cfg/leftcorner/chart/CfgLeftCornerChartRemove.java
@@ -37,71 +37,85 @@ public class CfgLeftCornerChartRemove
           && i + l1 == j && mayDottedRuleSplit[k].length() > 1) {
           this.name = "remove " + mayDottedRuleSplit[k].substring(1);
           if (k == mayDottedRuleSplit.length - 1) {
-            ChartItemInterface consequence = new DeductionChartItem(
-              ArrayUtils.getSubSequenceAsString(mayDottedRuleSplit, 0, k) + " "
-                + mayDottedRuleSplit[k].substring(1) + " •",
-              String.valueOf(i), String.valueOf(l1 + l2));
-            List<Tree> derivedTrees = new ArrayList<>();
-            if (Arrays.equals(antecedences.get(0).getItemForm(), itemForm1)) {
-              if (antecedences.get(1).getTrees().size() > 0) {
-                for (Tree tree1 : antecedences.get(0).getTrees()) {
-                  for (Tree tree2 : antecedences.get(1).getTrees()) {
-                    derivedTrees
-                      .add(TreeUtils.performLeftmostSubstitution(tree1, tree2));
-                  }
-                }
-              } else {
-                derivedTrees.addAll(antecedences.get(0).getTrees());
-              }
-            } else {
-              if (antecedences.get(0).getTrees().size() > 0) {
-                for (Tree tree1 : antecedences.get(0).getTrees()) {
-                  for (Tree tree2 : antecedences.get(1).getTrees()) {
-                    derivedTrees
-                      .add(TreeUtils.performLeftmostSubstitution(tree2, tree1));
-                  }
-                }
-              } else {
-                derivedTrees.addAll(antecedences.get(0).getTrees());
-              }
-            }
-            consequence.setTrees(derivedTrees);
-            logItemGeneration(consequence);
-            consequences.add(consequence);
+            handleDotBeforeLastSymbol(itemForm1, mayDottedRuleSplit, k, i, l1,
+                l2);
           } else {
-            ChartItemInterface consequence = new DeductionChartItem(
-              ArrayUtils.getSubSequenceAsString(mayDottedRuleSplit, 0, k) + " "
-                + mayDottedRuleSplit[k].substring(1) + " •"
-                + ArrayUtils.getSubSequenceAsString(mayDottedRuleSplit, k + 1,
-                  mayDottedRuleSplit.length),
-              String.valueOf(i), String.valueOf(l1 + l2));
-            List<Tree> derivedTrees = new ArrayList<>();
-            if (Arrays.equals(antecedences.get(0).getItemForm(), itemForm1)) {
-              for (Tree tree1 : antecedences.get(0).getTrees()) {
-                for (Tree tree2 : antecedences.get(1).getTrees()) {
-                  derivedTrees
-                    .add(TreeUtils.performLeftmostSubstitution(tree1, tree2));
-                }
-
-              }
-            } else {
-              for (Tree tree1 : antecedences.get(0).getTrees()) {
-                for (Tree tree2 : antecedences.get(1).getTrees()) {
-                  derivedTrees
-                    .add(TreeUtils.performLeftmostSubstitution(tree2, tree1));
-                }
-
-              }
-            }
-            consequence.setTrees(derivedTrees);
-            logItemGeneration(consequence);
-            consequences.add(consequence);
+            handleDotBeforeNotLastSymbol(itemForm1, mayDottedRuleSplit, k, i,
+                l1, l2);
           }
         } else {
           return;
         }
       }
     }
+  }
+
+  private void handleDotBeforeNotLastSymbol(String[] itemForm1,
+      String[] mayDottedRuleSplit, int k, int i, int l1, int l2)
+      throws ParseException {
+    ChartItemInterface consequence = new DeductionChartItem(
+      ArrayUtils.getSubSequenceAsString(mayDottedRuleSplit, 0, k) + " "
+        + mayDottedRuleSplit[k].substring(1) + " •"
+        + ArrayUtils.getSubSequenceAsString(mayDottedRuleSplit, k + 1,
+          mayDottedRuleSplit.length),
+      String.valueOf(i), String.valueOf(l1 + l2));
+    List<Tree> derivedTrees = new ArrayList<>();
+    if (Arrays.equals(antecedences.get(0).getItemForm(), itemForm1)) {
+      for (Tree tree1 : antecedences.get(0).getTrees()) {
+        for (Tree tree2 : antecedences.get(1).getTrees()) {
+          derivedTrees
+            .add(TreeUtils.performLeftmostSubstitution(tree1, tree2));
+        }
+
+      }
+    } else {
+      for (Tree tree1 : antecedences.get(0).getTrees()) {
+        for (Tree tree2 : antecedences.get(1).getTrees()) {
+          derivedTrees
+            .add(TreeUtils.performLeftmostSubstitution(tree2, tree1));
+        }
+
+      }
+    }
+    consequence.setTrees(derivedTrees);
+    logItemGeneration(consequence);
+    consequences.add(consequence);
+  }
+
+  private void handleDotBeforeLastSymbol(String[] itemForm1,
+      String[] mayDottedRuleSplit, int k, int i, int l1, int l2)
+      throws ParseException {
+    ChartItemInterface consequence = new DeductionChartItem(
+      ArrayUtils.getSubSequenceAsString(mayDottedRuleSplit, 0, k) + " "
+        + mayDottedRuleSplit[k].substring(1) + " •",
+      String.valueOf(i), String.valueOf(l1 + l2));
+    List<Tree> derivedTrees = new ArrayList<>();
+    if (Arrays.equals(antecedences.get(0).getItemForm(), itemForm1)) {
+      if (antecedences.get(1).getTrees().size() > 0) {
+        for (Tree tree1 : antecedences.get(0).getTrees()) {
+          for (Tree tree2 : antecedences.get(1).getTrees()) {
+            derivedTrees
+              .add(TreeUtils.performLeftmostSubstitution(tree1, tree2));
+          }
+        }
+      } else {
+        derivedTrees.addAll(antecedences.get(0).getTrees());
+      }
+    } else {
+      if (antecedences.get(0).getTrees().size() > 0) {
+        for (Tree tree1 : antecedences.get(0).getTrees()) {
+          for (Tree tree2 : antecedences.get(1).getTrees()) {
+            derivedTrees
+              .add(TreeUtils.performLeftmostSubstitution(tree2, tree1));
+          }
+        }
+      } else {
+        derivedTrees.addAll(antecedences.get(0).getTrees());
+      }
+    }
+    consequence.setTrees(derivedTrees);
+    logItemGeneration(consequence);
+    consequences.add(consequence);
   }
 
   @Override public String toString() {
