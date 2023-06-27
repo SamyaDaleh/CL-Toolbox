7fb43f0604b2637a7a630938b8c04b65c53bb447
SamyaDaleh <samya.daleh@freenet.de>
2023-06-27 13:07:38 +0200

    Pcfg rule extends cfg rule

--- src/main/java/com/github/samyadaleh/cltoolbox/common/cfg/PcfgProductionRule.java
+++ src/main/java/com/github/samyadaleh/cltoolbox/common/cfg/PcfgProductionRule.java
@@ -6,24 +6,20 @@ import java.text.ParseException;
  * Representation of a context-free rule where the lhs is only allowed to
  * contain one symbol and the rule has a probability.
  */
-public class PcfgProductionRule {
-  private Double p;
-  private final String lhs;
-  private final String[] rhs;
+public class PcfgProductionRule extends CfgProductionRule {
+  private final Double p;
 
   /**
    * Construction of an array of length 3 where the elements are lhs, rhs and
    * probability.
    */
   PcfgProductionRule(String[] rule) {
-    this.lhs = rule[0];
-    this.rhs = rule[1].split(" ");
+    super(rule[0], rule[1].split(" "));
     this.p = Double.parseDouble(rule[2]);
   }
 
   PcfgProductionRule(String lhs, String[] rhs, double p) {
-    this.lhs = lhs;
-    this.rhs = rhs;
+    super(lhs, rhs);
     this.p = p;
   }
 
@@ -31,34 +27,14 @@ public class PcfgProductionRule {
    * Creates a rule from a String representation like 0.5 : S -> A B
    */
   PcfgProductionRule(String ruleString) throws ParseException {
-    if (!ruleString.contains("->")) {
-      throw new ParseException("Separator -> missing in rule " + ruleString, 0);
-    }
-    if (!ruleString.contains(":")) {
-      throw new ParseException("Separator : missing in rule " + ruleString, 0);
-    }
+    super(ruleString.split(":", 2)[1]);
     if (ruleString.indexOf("->") < ruleString.indexOf(':')) {
       throw new ParseException(": has to be left of -> in rule " + ruleString,
         0);
     }
     String[] ruleSplit = ruleString.split(":", 2);
     this.p = Double.parseDouble(ruleSplit[0].trim());
-    String[] ruleSplit2 = ruleSplit[1].split("->", 2);
-    this.lhs = ruleSplit2[0].trim();
-    if (ruleSplit2[1].trim().equals("") || ruleSplit2[1].trim().equals("ε")) {
-      this.rhs = new String[] {""};
-    } else {
-      this.rhs = ruleSplit2[1].trim().split(" ");
     }
-  }
-
-  public String getLhs() {
-    return this.lhs;
-  }
-
-  public String[] getRhs() {
-    return this.rhs;
-  }
 
   public Double getP() {
     return this.p;
@@ -66,10 +42,11 @@ public class PcfgProductionRule {
 
   @Override public String toString() {
     double roundedP = Math.round(p * 100.0) / 100.0;
-    if (rhs[0].equals("")) {
-      return roundedP + " : " + lhs + " -> ε";
+    if (this.getRhs()[0].equals("")) {
+      return roundedP + " : " + this.getLhs() + " -> ε";
     } else {
-      return roundedP + " : " + lhs + " -> " + String.join(" ", rhs);
+      return roundedP + " : " + this.getLhs() + " -> "
+          + String.join(" ", this.getRhs());
     }
   }
 }
